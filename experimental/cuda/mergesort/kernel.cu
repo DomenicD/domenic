18a74cbccee91cab3b6094aea6022b38c3ce2acb
Domenic Donato <info@domenicdonato.com>
2016-03-06 11:05:23 -0800

    Switched from local to shared memory in cuda merge sort and implemented the cpu version for performance comparison

--- experimental/cuda/mergesort/kernel.cu
+++ experimental/cuda/mergesort/kernel.cu
@@ -8,49 +8,48 @@
 #include <iomanip>
 #include <sstream>
 #include <algorithm>
+#include <chrono>
 
 using namespace std;
 
-__global__ void merge_sort(float *arr, int length, int chunk)
+__global__ void merge_sort(float *d_unsorted_arr, float *d_sorted_arr, unsigned long long length, unsigned long long chunk)
 {
-	int start = (blockIdx.x * blockDim.x + threadIdx.x) * chunk;
+	unsigned long long start = (blockIdx.x * blockDim.x + threadIdx.x) * chunk;
 	if (start >= length)
 	{
 		return;
 	}
 
-	int middle = min(start + chunk / 2, length);
-	int end = min(start + chunk, length);
-	int left = start;
-	int right = middle;
-	int i = 0;
-	float* temp = new float[chunk]{0.f};
+	unsigned long long middle = min(start + chunk / 2, length);
+	unsigned long long end = min(start + chunk, length);
+	unsigned long long left = start;
+	unsigned long long right = middle;
+	unsigned long long index = start;
 
 	while (left < middle || right < end)
 	{
 		float result;
 		if (left < middle && right < end)
 		{
-			result = arr[left] <= arr[right] ? arr[left++] : arr[right++];
+			result = d_unsorted_arr[left] <= d_unsorted_arr[right] ? d_unsorted_arr[left++] : d_unsorted_arr[right++];
 		} else 
 		{
-			result = left < middle ? arr[left++] : arr[right++];
+			result = left < middle ? d_unsorted_arr[left++] : d_unsorted_arr[right++];
 		}
-		temp[i++] = result;
-	}	
-
-	for (int index = 0; start + index < end; index++)
-	{
-		arr[start + index] = temp[index];
+		d_sorted_arr[index++] = result;
 	}
-	delete [] temp;
+
+	for (unsigned long long i = start; i < end; i++)
+	{
+		d_unsorted_arr[i] = d_sorted_arr[i];
+	}
 }
 
-void printArray(float* arr, const int length)
+void printArray(float* arr, const unsigned long long length)
 {
 	std::stringstream ss;
 	ss << "[ ";
-	for (int i = 0; i < length; i++)
+	for (unsigned long long i = 0; i < length; i++)
 	{
 		ss << arr[i] << ", ";
 	}
@@ -59,33 +58,40 @@ void printArray(float* arr, const int length)
 	std::cout << str << " ]" << std::endl;
 }
 
-int main()
+string timed_operation;
+chrono::system_clock::time_point start_time;
+
+void startTimer(string operation)
 {
-	const int length = 100000;
-	const int size = length * sizeof(float);
+	timed_operation = operation;
+	start_time = chrono::steady_clock::now();
+}
 
-	cudaError_t cudaStatus;
-	curandStatus_t curandStatus;
-	curandGenerator_t gen;
-	curandStatus = curandCreateGenerator(&gen, CURAND_RNG_PSEUDO_MTGP32);
-	curandSetPseudoRandomGeneratorSeed(gen, time(0));
+void stopTimer()
+{
+	auto end = chrono::steady_clock::now();
+	auto diff = end - start_time;
+	cout << timed_operation << " took " << chrono::duration <double, milli>(diff).count() << " ms" << endl;
+}
 
-	float* arr;
-	cudaStatus = cudaMalloc(&arr, size);
-	curandStatus = curandGenerateUniform(gen, arr, length);
-
-	int chunk = 2;
+void cudaMergeSort(float *d_unsorted_array, float *d_sorted_array, unsigned long long length)
+{
+	unsigned long long chunk = 2;
 	bool isSorted = false;
+	const int threads_per_block = 512;
+	//const int threads_per_block = 256;
+	//const int threads_per_block = 32;
 	while (!isSorted)
 	{
-		int threads = ceilf(length / float(chunk));
-		int grids = ceilf(threads / 32.f);
+		unsigned long long threads = ceilf(length / float(chunk));
+		unsigned long long grids = ceilf(threads / float(threads_per_block));
 		if (grids > 0)
 		{
-			merge_sort << <grids, 32 >> >(arr, length, chunk);
-		} else
+			merge_sort << <grids, threads_per_block >> >(d_unsorted_array, d_sorted_array, length, chunk);
+		}
+		else
 		{
-			merge_sort << <1, threads >> >(arr, length, chunk);
+			merge_sort << <1, threads >> >(d_unsorted_array, d_sorted_array, length, chunk);
 		}
 		if (chunk >= length)
 		{
@@ -93,19 +99,120 @@ int main()
 		}
 		chunk *= 2;
 	}
+}
 
-	float sorted [length];
-	cudaMemcpy(sorted, arr, size, cudaMemcpyDeviceToHost);	
-	bool isCorrect = true;
-	int i = 0;
-	while (isCorrect && i < length - 1)
+void cpuMergeSort(float *h_unsorted_array, float *h_sorted_array, unsigned long long start, unsigned long long chunk, unsigned long long length)
+{
+	unsigned long long middle = min(start + chunk / 2, length);
+	unsigned long long end = min(start + chunk, length);
+	unsigned long long left = start;
+	unsigned long long right = middle;
+	unsigned long long index = start;
+
+	while (left < middle || right < end)
 	{
-		isCorrect = sorted[i] <= sorted[i + 1];
-		i++;
+		float result;
+		if (left < middle && right < end)
+		{
+			result = h_unsorted_array[left] <= h_unsorted_array[right] ? h_unsorted_array[left++] : h_unsorted_array[right++];
+		}
+		else
+		{
+			result = left < middle ? h_unsorted_array[left++] : h_unsorted_array[right++];
+		}
+		h_sorted_array[index++] = result;
 	}
 
-	cout << "List size: " << length << ", Is correct: " << isCorrect << endl;
-	// printArray(sorted, length);
+	for (unsigned long long i = start; i < end; i++)
+	{
+		h_unsorted_array[i] = h_sorted_array[i];
+	}
+}
+
+void cpuMergeSort(float *h_unsorted_array, float *h_sorted_array, unsigned long long length)
+{
+	unsigned long long chunk = 2;
+	bool isSorted = false;
+	while (!isSorted)
+	{
+		unsigned long long threads = ceilf(length / float(chunk));
+		for (unsigned long long i = 0; i < threads; i++)
+		{
+			cpuMergeSort(h_unsorted_array, h_sorted_array, i * chunk, chunk, length);
+		}
+		if (chunk >= length)
+		{
+			isSorted = true;
+		}
+		chunk *= 2;
+	}
+}
+
+void checkSortedArray(float *sorted_array, unsigned long long length)
+{
+	bool isCorrect = true;
+	unsigned long long i = 0;
+	while (isCorrect && i < length - 1)
+	{
+		isCorrect = sorted_array[i] <= sorted_array[i + 1];
+		i++;
+	}
+	cout << "List size: " << length << ", Is correct: " << (isCorrect ? "Yes" : "No") << endl;
+}
+
+// TODO(domenicd): implement a third version in OpenMP and run on Chapman 
+// numa server to compare CUDA and OpenMP.
+int main()
+{
+	//unsigned long long length = 1610612736/2;
+	unsigned long long length = 10000000*5; // CPU takes 15 seconds at this length
+	unsigned long long size = length * sizeof(float);
+
+	cudaError_t cudaStatus;
+	curandStatus_t curandStatus;
+	curandGenerator_t gen;
+	curandStatus = curandCreateGenerator(&gen, CURAND_RNG_PSEUDO_MTGP32);
+	curandSetPseudoRandomGeneratorSeed(gen, time(0));
+	chrono::system_clock::time_point end;
+
+	float* d_unsorted_array;
+	float* d_sorted_array;
+	cudaStatus = cudaMalloc(&d_unsorted_array, size);
+	cudaStatus = cudaMalloc(&d_sorted_array, size);
+	
+	startTimer("Random number generation");
+	curandStatus = curandGenerateUniform(gen, d_unsorted_array, length);
+	cudaDeviceSynchronize();
+	stopTimer();
+
+	// Store the same sequence of random numbers to use in all tests
+	float * h_unsorted_array = new float[length];
+	cudaMemcpy(h_unsorted_array, d_unsorted_array, size, cudaMemcpyDeviceToHost);
+
+	startTimer("CUDA sorting");
+	cudaMergeSort(d_unsorted_array, d_sorted_array, length);	
+	// Copy from device and check result
+	float * h_sorted_array = new float[length];
+	cudaMemcpy(h_sorted_array, d_unsorted_array, size, cudaMemcpyDeviceToHost);
+	stopTimer();
+	checkSortedArray(h_sorted_array, length);
+
+	// Clean up
+	cudaFree(d_sorted_array);
+	cudaFree(d_unsorted_array);
+	delete [] h_sorted_array;
+
+	// Reallocate h_sorted_array and perform sorting on CPU
+	h_sorted_array = new float[length];
+	startTimer("CPU sorting");
+	cpuMergeSort(h_unsorted_array, h_sorted_array, length);
+	stopTimer();
+	checkSortedArray(h_sorted_array, length);
+
+	// Clean up
+	delete [] h_sorted_array;
+	delete [] h_unsorted_array;
+
 	getchar();
     return 0;
 }
