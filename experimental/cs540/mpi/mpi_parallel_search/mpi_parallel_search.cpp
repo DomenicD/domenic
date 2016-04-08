d6e65aea3d23f1c1bb3145d210890da3e96dee34
Domenic Donato <info@domenicdonato.com>
2016-04-08 09:32:05 -0700

    Completed mpi parallel search

--- experimental/cs540/mpi/mpi_parallel_search/mpi_parallel_search.cpp
+++ experimental/cs540/mpi/mpi_parallel_search/mpi_parallel_search.cpp
@@ -4,7 +4,10 @@
 #include <string>
 #include <vector>
 
+const std::string FILE_NAME = "random_numbers.txt";
+const std::string SMALL_FILE_NAME = "random_numbers_small.txt";
 const int TARGET_INDICES_SEND = 1;
+const int ROOT = 0;
 
 int main(int argc, char *argv[]) {
   MPI_Init(&argc, &argv);
@@ -20,10 +23,11 @@ int main(int argc, char *argv[]) {
   // http://www.cplusplus.com/doc/tutorial/files/
 
   int items_per_process = -1;
-  int target_value;
+  int target_value = 0;
   std::vector<int> numbers;
-  if (rank == 0) {
-    std::string file_name = "random_numbers.txt";
+  // Have root read in the file.
+  if (rank == ROOT) {
+    std::string file_name = FILE_NAME;
     std::ifstream data_file(file_name, std::ios_base::in);
 
     if (!data_file.is_open()) {
@@ -39,11 +43,12 @@ int main(int argc, char *argv[]) {
     }
   }
 
-  MPI_Bcast(&target_value, 1, MPI_INT, 0, MPI_COMM_WORLD);
-  MPI_Bcast(&items_per_process, 1, MPI_INT, 0, MPI_COMM_WORLD);
+  // Send the target_value and items_per_process to every processor.
+  MPI_Bcast(&target_value, 1, MPI_INT, ROOT, MPI_COMM_WORLD);
+  MPI_Bcast(&items_per_process, 1, MPI_INT, ROOT, MPI_COMM_WORLD);
 
   if (items_per_process < 0) {
-    if (rank == 0) {
+    if (rank == ROOT) {
       std::cout << "FAILURE EXIT" << std::endl;
     }
     exit(EXIT_FAILURE);
@@ -51,17 +56,20 @@ int main(int argc, char *argv[]) {
 
   int *local_numbers = new int[items_per_process];
 
-  MPI_Scatter(&numbers[0], items_per_process, MPI_INT, local_numbers,
-              items_per_process, MPI_INT, 0, MPI_COMM_WORLD);
+  // Send each processor a part of the array.
+  MPI_Scatter(&numbers.front(), items_per_process, MPI_INT, local_numbers,
+              items_per_process, MPI_INT, ROOT, MPI_COMM_WORLD);
 
+  // Search for the target_value.
   std::vector<int> local_target_indices;
   for (size_t i = 0; i < items_per_process; i++) {
     if (local_numbers[i] == target_value) {
-      local_target_indices.push_back(i * items_per_process + i);
+      local_target_indices.push_back(rank * items_per_process + i);
     }
   }
 
-  if (rank == 0) {
+  // Consolidate the results.
+  if (rank == ROOT) {
     std::vector<int> target_indices;
     target_indices.insert(target_indices.begin(), local_target_indices.begin(),
                           local_target_indices.end());
@@ -78,10 +86,17 @@ int main(int argc, char *argv[]) {
       target_indices.insert(target_indices.begin() + target_indices.size(),
                             process_targets, process_targets + count);
     }
-    // TODO(domenic): Need to print out the target_indices.
+    delete[] process_targets;
+
+    for (int index : target_indices) {
+      std::cout << index << std::endl;
+    }
   } else {
-    // TODO(domenic): Need to send messaged to the root node.
+    MPI_Ssend(&local_target_indices.front(), local_target_indices.size(),
+              MPI_INT, ROOT, TARGET_INDICES_SEND, MPI_COMM_WORLD);
   }
+
+  delete[] local_numbers;
 
   return 0;
 }
