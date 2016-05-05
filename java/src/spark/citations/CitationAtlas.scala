f329e55f9811af7984905c472b9364bd857bb744
Domenic Donato <info@domenicdonato.com>
2016-05-05 14:55:43 -0700

    Inital go at calcualting the relavence of citations in a reference graph

--- java/src/spark/citations/CitationAtlas.scala
+++ java/src/spark/citations/CitationAtlas.scala
@@ -1,8 +1,10 @@
 package spark.citations
 
 import org.apache.spark.{SparkConf, SparkContext}
-import org.apache.spark.graphx.{Edge, Graph}
+import org.apache.spark.graphx._
 import org.apache.spark.rdd.RDD
+
+import scala.collection.mutable
 
 object CitationAtlas {
   def main(args: Array[String]): Unit = {
@@ -10,7 +12,7 @@ object CitationAtlas {
     val sc = new SparkContext(conf)
 
     val citations: RDD[List[Long]] = sc.textFile(
-      "file:///home/djdonato/git/domenic/java/src/spark/citations/Cit-HepPh.txt", 1)
+      "file:///home/djdonato/git/domenic/java/src/spark/citations/test_data.txt", 1)
       .map(s => s.split("\\s"))
       .filter(strings => strings.length == 2)
       .map(strings => List(strings(0).toLong, strings(1).toLong))
@@ -21,17 +23,88 @@ object CitationAtlas {
       .distinct
       .map(id => (id, new Publication(id)))
 
-    val edges: RDD[Edge[CitationMetadata]] = citations.map(longs => new Edge(longs.head, longs(1),
-      new CitationMetadata()))
+    val edges: RDD[Edge[Boolean]] = citations.map(longs => new Edge(longs.head, longs(1), false))
 
-    val graph: Graph[Publication, CitationMetadata] = Graph.apply(vertices, edges)
+    val graph: Graph[Publication, Boolean] = Graph.apply(vertices, edges)
 
-    val ranks = graph.pageRank(.001).vertices
+    val referenceMap: Map[VertexId, Set[VertexId]] =
+      graph.collectNeighborIds(EdgeDirection.Out).collect()
+        .groupBy(_._1)
+        .mapValues(_.flatMap(v => v._2).toSet)
+        // need to .map(identity) because of scala bug
+        // http://stackoverflow.com/questions/32900862/map-can-not-be-serializable-in-scala
+        .map(identity)
 
-    ranks.sortBy(r => r._2).take(10).foreach(r => println("Vertex: " + r._1 + " Rank: " + r._2))
+    val metadata = graph.vertices.mapValues((id, publication) =>
+      getReferenceRanking(referenceMap, publication))
+
+    metadata.foreach(m => print(m))
+  }
+
+  def getReferenceRanking(refMap: Map[VertexId, Set[VertexId]],
+                          publication: Publication): CitationMetadata = {
+    val rootId = publication.id
+    val ranks = new mutable.HashMap[VertexId, ReferenceRank]()
+    ranks.put(rootId, new ReferenceRank(rootId, 0))
+
+    val visited = new mutable.HashSet[VertexId]()
+    visited.add(rootId)
+
+    val queue = new mutable.Queue[VertexId]()
+    val maxDepth = 10
+    var depth = 1
+    recordCitations(ranks, queue, depth, rootId, refMap.get(rootId).get)
+    
+    while (queue.nonEmpty && depth < maxDepth) {
+      val currentId = queue.dequeue()
+      if (!visited.contains(currentId)) {
+        recordCitations(ranks, queue, depth, currentId, refMap.get(currentId).get)
+        visited.add(currentId)
+        depth += 1
+      }
+    }
+
+    // Compute the citationQuality by taking the average of the top n Publications that cite this
+    // Publication.
+    ranks.values.foreach(rank => {
+      val n = 3
+      rank.citationQuality = rank.citations
+        .map(vertexId => ranks.get(vertexId).get.citationCount)
+        .toList.sorted(Ordering[Long].reverse)
+        .take(n)
+        .sum / n
+    })
+
+    ranks.remove(rootId)
+
+    new CitationMetadata(publication,
+      ranks.values.toList.sortBy(x => (-x.citationCount, -x.citationQuality, x.depth)))
+  }
+
+  def recordCitations(ranks: mutable.HashMap[VertexId, ReferenceRank],
+                      queue: mutable.Queue[VertexId],
+                      depth: Long,
+                      parentId: VertexId,
+                      childrenIds: Set[VertexId]): Unit = {
+    childrenIds.foreach(id => {
+      if (!ranks.contains(id)) {
+        ranks.put(id, new ReferenceRank(id, depth))
+      }
+      val rank = ranks.get(id)
+      rank.get.citationCount += 1
+      rank.get.citations.add(parentId)
+      queue.enqueue(id)
+    })
   }
 }
 
-class CitationMetadata extends Serializable {}
+class Publication(val id: Long) extends Serializable { }
 
-class Publication(val id: Long) extends Serializable {}
+class CitationMetadata(val publication: Publication,
+                       val referenceRanks: List[ReferenceRank]) extends Serializable { }
+
+class ReferenceRank(val id: VertexId, val depth: Long) extends Serializable {
+  var citationCount: Long = 0
+  var citationQuality: Double = 0.0
+  val citations: mutable.Set[VertexId] = new mutable.HashSet[VertexId]()
+}
