05caa3fd4068a0ad449173a55e2b010c17a20174
Domenic Donato <info@domenicdonato.com>
2016-05-07 15:40:43 -0700

    Beta UI for Citation Atlas

--- java/src/spark/citations/ui/src/app.ts
+++ java/src/spark/citations/ui/src/app.ts
@@ -36,11 +36,11 @@ class CitationController {
     svg: d3.Selection<any>;
     links: d3.Selection<any>;
     nodes: d3.Selection<any>;
-    force: d3.layout.Force<d3.layout.force.Link<d3.layout.force.Node>, d3.layout.force.Node>;
+
     constructor() {
         this.citations = data;
         this.citationIds = Object.keys(data).map(Number);
-        this.citationId = 10102; //this.citationIds[0];
+        this.citationId = this.citationIds[0];
     }
 
     get citationId(): number {
@@ -55,50 +55,80 @@ class CitationController {
         }
     }
 
-    onTick() {
-    this.links.attr("x1", function(d) { return d.source.x; })
-        .attr("y1", function(d) { return d.source.y; })
-        .attr("x2", function(d) { return d.target.x; })
-        .attr("y2", function(d) { return d.target.y; });
-
-    this.nodes.attr("cx", function(d) { return d.x; })
-        .attr("cy", function(d) { return d.y; });
-    }
-
     // TODO(domenicd): Use focus point so pull the depths apart.
     createForceGraph() {
         let nodeMap: {[key: number]: CitationNode} = {};
         nodeMap[this.citation.id] = CitationNode.emptyNode(this.citation.id);
         let maxCount = 0;
+        let maxDepth = 0;
+        let maxQuality = 0;
+        let nodesAtDepth: {[depth: number]: number} = {};
 
         for (let rank of this.citation.ranks) {
             nodeMap[rank.id] = new CitationNode(rank);
-            if (rank.count > maxCount) {
-                maxCount = rank.count;
+            // Compute global attributes.
+            if (rank.count > maxCount) maxCount = rank.count;
+            if (rank.depth > maxDepth) maxDepth = rank.depth;
+            if (rank.quality > maxQuality) maxQuality = rank.quality;
+            if (!nodesAtDepth[rank.depth]) {
+                nodesAtDepth[rank.depth] = 1;
+            } else {
+                nodesAtDepth[rank.depth]++;
             }
+
             for (let id of rank.citations) {
                 if (!nodeMap[id]) {
                     nodeMap[id] = CitationNode.emptyNode(id);
                 }
+                nodeMap[id].children.push(nodeMap[rank.id]);
             }
         }
 
-        let links: CitationLink[] = [];
+        let citationLinks: CitationLink[] = [];
 
         for (let rank of this.citation.ranks) {
             let rankNode = nodeMap[rank.id];
             for (let sourceId of rank.citations) {
-                links.push(new CitationLink(nodeMap[sourceId], rankNode))
+                citationLinks.push(new CitationLink(nodeMap[sourceId], rankNode))
             }
         }
 
-        let nodes: CitationNode[] = [];        
+        let citationNodes: CitationNode[] = [];
 
-        for (let id of Object.keys(nodeMap)) {
-            nodes.push(nodeMap[Number(id)]);
+        let depthIndices: number[] = [];
+
+        for (let i = 0; i <= maxDepth; i++) {
+            depthIndices.push(0);
         }
 
-        if (this.svg) {
+        for (let id of Object.keys(nodeMap)) {
+            var node = nodeMap[Number(id)];
+            node.maxCount = maxCount;
+            if (node.depthIndex === -1) {
+                node.depthIndex = depthIndices[node.depth]++;
+            }
+            citationNodes.push(node);
+        }
+
+        let yScale = d3.scale.ordinal()
+            .domain(<any>(d3.range(maxDepth + 1)))
+            .rangeRoundPoints([0, D3_HEIGHT], .5);
+
+        // TODO(domenicd): Use nodesAtDepth to create a scale for each
+        let xScales = depthIndices.map(d =>
+            d3.scale.linear()
+                .domain([-1, d])
+                .range([0, D3_WIDTH]));
+
+        let colorScale = d3.scale.linear().domain([0, maxQuality]).range(["blue", "red"]);
+
+        let onTick = this.getTickFn(
+            (node: CitationNode) => xScales[node.depth](node.depthIndex),
+            (node: CitationNode) => yScale(node.depth),
+            (node: CitationNode) => colorScale(node.quality));
+
+
+        if (this.svg) {            
             this.svg.remove();
         }
 
@@ -106,42 +136,62 @@ class CitationController {
             .attr("width", D3_WIDTH)
             .attr("height", D3_HEIGHT);
 
-        this.links = this.svg.selectAll(".link");
-        this.nodes = this.svg.selectAll(".node");
-
-        this.force = d3.layout.force()
-            .size([D3_WIDTH, D3_HEIGHT])
-            .on("tick", () => this.onTick());
-
-        this.force
-            .nodes(nodes)
-            .links(links)
-            //.gravity(0)
-            //.linkDistance(D3_HEIGHT/6)
-            .charge((n: CitationNode) => -100 * (n.count/maxCount) * CitationNode.RADIUS)
-            .linkStrength(.1)
-            .start();
-
-        this.links = this.links.data(links)
+        this.links = this.svg.selectAll(".link")
+            .data(citationLinks)
             .enter()
             .append("line")
             .attr("class", "link");
 
-        this.nodes = this.nodes.data(nodes)
+        this.nodes = this.svg.selectAll(".node")
+            .data(citationNodes)
             .enter().append("circle")
             .attr("class", "node")
-            .attr("r", (n) => 12 + (n.count/maxCount) * CitationNode.RADIUS);
+            .attr("r", (n) => 5 + (n.count/maxCount) * CitationNode.RADIUS);
+
+        d3.layout.force()
+            .nodes(citationNodes)
+            .links(citationLinks)
+            .size([D3_WIDTH, D3_HEIGHT])
+            .charge(0)
+            .linkStrength(0)
+            .on("tick", onTick)
+            .start();
+    }
+
+    getTickFn(xDomain: (node: CitationNode) => number,
+              yDomain: (node: CitationNode) => number,
+              color: (node: CitationNode) => string): Function {
+        // TODO(domenicd): Can use link count to alter Y axis by a little.
+        return () => {
+            this.nodes
+                .attr("cx", (n) => xDomain(n))
+                .attr("cy", (n) => this.getY(yDomain, n))
+                .style("fill", color);
+
+            this.links
+                .attr("x1", (l) => xDomain(l.source))
+                .attr("y1", (l) => this.getY(yDomain, l.source))
+                .attr("x2", (l) => xDomain(l.target))
+                .attr("y2", (l) => this.getY(yDomain, l.target));
+        };
+    }
+
+    getY(yDomain: (node: CitationNode) => number, node: CitationNode) {
+        let y = yDomain(node);
+        y += (node.count / node.maxCount) * -CitationNode.RADIUS;
+        return y;
     }
 }
 
-class CitationNode {
+class CitationNode implements d3.layout.force.Node {
     id: number;
     count: number;
     quality: number;
     depth: number;
+    maxCount: number;
+    depthIndex: number = -1;
 
-    parent: CitationNode = null;
-    children: CitationNode[];
+    children: CitationNode[] = [];
 
     constructor(rank: CitationRank) {
         this.id = rank.id;
@@ -150,7 +200,7 @@ class CitationNode {
         this.depth = rank.depth;
     }
 
-    static get RADIUS() { return 30; }
+    static get RADIUS() { return 50; }
 
     static emptyNode(id: number): CitationNode {
         return new CitationNode({
