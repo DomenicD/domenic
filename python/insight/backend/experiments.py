21bddf670ef7d5e51348156dcf08b1d1478b235d
Domenic Donato <info@domenicdonato.com>
2016-11-29 08:32:47 -0800

    Switched from writing new dataset per epoch to appending to existing dataset.

--- python/insight/backend/experiments.py
+++ python/insight/backend/experiments.py
@@ -1,7 +1,9 @@
-from typing import Sequence, Mapping
+from typing import Sequence, Mapping, Tuple
 
 import numpy as np
 import h5py
+import time
+
 import modeling.assembled_models as am
 from modeling.domain_objects import ParameterSet
 from modeling.layers import QuadraticLayer
@@ -29,51 +31,65 @@ def close_file(file: h5py.File):
     file.close()
 
 
-def write_deltas(group: h5py.Group, epoch: str, params: ParameterSet):
-    group.require_group("deltas").require_group("values").require_dataset(
-        epoch, data=np.reshape([delta.value for delta in params.deltas.flatten()],
-                               params.deltas.shape))
-    # group.create_group("deltas").create_group("steps").require_dataset(epoch,)
+def get_dataset(file: h5py.Group, name: str, rows: int, data_shape: Tuple[int] = (),
+                data_type: np.dtype = np.dtype(float)):
+    return file.require_dataset(name, (rows,) + data_shape, data_type)
 
 
-def write_parameters(file: h5py.File, epoch: str, parameters: Sequence[Mapping[str, ParameterSet]]):
+def write_parameters(file: h5py.File, rows: int, epoch: int,
+                     parameters: Sequence[Mapping[str, ParameterSet]]):
+    param_group = file.require_group(Group.PARAMETERS)
     for layer in range(len(parameters)):
-        layer_group = file[Group.PARAMETERS].require_group("layer").require_group(str(layer))
         for params in parameters[layer].values():
-            param_group = layer_group.require_group("parameter_set").require_group(params.name)
-            param_group.require_group("values").require_dataset(epoch, data=params.values)
-            param_group.require_group("gradients").require_dataset(epoch, data=params.gradients)
-            write_deltas(param_group, epoch, params)
+            get_dataset(param_group.require_group(params.name), "values", rows,
+                        np.shape(params.values))[epoch] = params.values
+
+            get_dataset(param_group.require_group(params.name), "gradients", rows,
+                        np.shape(params.gradients))[epoch] = params.gradients
+
+            delta_values = np.reshape([delta.value for delta in params.deltas.flatten()],
+                                      params.deltas.shape)
+            get_dataset(param_group.require_group(params.name), "delta_values", rows,
+                        np.shape(delta_values))[epoch] = delta_values
+
+            # TODO: Record the delta steps.
 
 
-def write_batch_result(file: h5py.File, total_epochs: int, result: BatchResult):
+def write_batch_result(file: h5py.File, rows: int, result: BatchResult):
     epoch = result.batch_number - 1
-    # TODO: clean up the dataset creation/require.
-    file.require_dataset(Group.TOTAL_ERROR, (total_epochs,), np.dtype(float))[epoch] = result.total_error
-    file.require_dataset(Group.AVERAGE_ERROR, (total_epochs,), np.dtype(float))[epoch] = result.avg_error
-    # TODO: Update write_parameters to append to dataset rather than creating a new one for each epoch.
-    # write_parameters(file, epoch, result.parameters)
-    file.require_dataset(Group.INPUTS, (total_epochs,) + np.shape(result.inputs), np.dtype(float))[epoch] = result.inputs
-    file.require_dataset(Group.EXPECTED, (total_epochs,) + np.shape(result.expected), np.dtype(float))[epoch] = result.expected
-    file.require_dataset(Group.ACTUAL, (total_epochs,) + np.shape(result.actual), np.dtype(float))[epoch] = result.actual
+    get_dataset(file, Group.TOTAL_ERROR, rows)[epoch] = result.total_error
+    get_dataset(file, Group.AVERAGE_ERROR, rows)[epoch] = result.avg_error
+    write_parameters(file, rows, epoch, result.parameters)
+    get_dataset(file, Group.INPUTS, rows, np.shape(result.inputs))[epoch] = result.inputs
+    get_dataset(file, Group.EXPECTED, rows, np.shape(result.expected))[epoch] = result.expected
+    get_dataset(file, Group.ACTUAL, rows, np.shape(result.actual))[epoch] = result.actual
 
 
 def quad():
-    epochs = 100
+    start_time = time.time()
+    epochs = 1000
     layers = [1, 2, 1]
     network = am.feed_forward_network(QuadraticLayer, layers, 'SimpleUpdater')
-    batch_size = 50
+    batch_size = 100
     trainer = ClosedFormFunctionTrainer(network, lambda x: x ** 2, (-10, 10), batch_size)
-    file = file = h5py.File('test.h5', 'w')
+    file = h5py.File('test.h5', 'w')
     file.require_group(Group.CONFIGURATION).create_dataset(Dataset.EPOCHS, data=epochs)
     file.require_group(Group.CONFIGURATION).create_dataset(Dataset.BATCH_SIZE, data=batch_size)
     file.require_group(Group.CONFIGURATION).create_dataset(Dataset.LAYERS, data=layers)
+    stop_time = time.time()
+    print("setup:", stop_time - start_time)
 
+    start_time = time.time()
     for i in range(epochs):
         result = trainer.batch_train()
         write_batch_result(file, epochs, result)
+    stop_time = time.time()
+    print("experiment", stop_time - start_time)
 
+    start_time = time.time()
     close_file(file)
+    stop_time = time.time()
+    print("output:", stop_time - start_time)
 
 
 if __name__ == "__main__":
